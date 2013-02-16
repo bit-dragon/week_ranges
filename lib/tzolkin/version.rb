6e0b58d7d310a10a38256caffb7793f2ce93917e
gmjorge <jorge.em223@gmail.com>
2013-02-15 17:24:31 -0700

    Removed old files

--- lib/tzolkin/version.rb
+++
@@ -1,7 +0,0 @@
-module Tzolkin
-  VERSION = "0.0.1"
-
-	def self.version_string
-		"Version #{VERSION}"
-	end
-end
--- spec/lib/tzolkin/version_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-require 'tzolkin/version'
-
-describe Tzolkin do
-	it '#version_string' do
-		Tzolkin::version_string.should eq('Version 0.0.1')
-	end
-end
